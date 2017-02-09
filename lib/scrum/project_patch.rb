d0f1f2969bcb2cd17bb45bf066c971bcea4cc535
Ihor Savchenko <redden.tears@gmail.com>
2017-02-09 17:17:17 +0200

    Fix scrumban compability with rails 4

--- lib/scrum/project_patch.rb
+++ lib/scrum/project_patch.rb
@@ -8,7 +8,7 @@ module Scrum
       base.class_eval do
 
         has_one :product_backlog, class_name: 'ProductBacklog'
-        has_many :sprints, class_name: 'Sprint', :dependent => :delete_all, :order => "start_date ASC, name ASC"
+        has_many :sprints, -> { order(start_date: :asc, name: :asc) }, class_name: 'Sprint', dependent: :delete_all
 
         #def last_sprint
         #  sprints.sort{|a, b| a.end_date <=> b.end_date}.last
